942b7fe19923d8ec0fe1bfbccd1fbe07cd8ce067
aefayoub <aef.ayoub@gmail.com>
2024-12-04 18:47:04 +0100

    Edit global configurations, global urls

--- quotesV3/urls.py
+++ quotesV3/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for quotesV3 project.
+URL configuration for quotes project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +15,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
+    
+    # Add Django site authentication urls (for login, logout, password management)
+    path('accounts/', include('django.contrib.auth.urls')),
+
     path('admin/', admin.site.urls),
-]
+        
+    # APIs
+    path('api/', include("api.urls.feeds")),
+    path('api/', include("api.urls.auth")),
+    
+    # Internationalization
+    path('i18n/', include('django.conf.urls.i18n')),
+    
+    # Serve the Frontend app
+    path('feeds/', TemplateView.as_view(template_name='index.html')),
+    path('api/token-auth/', obtain_auth_token),
+    ]
