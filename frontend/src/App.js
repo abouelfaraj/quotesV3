0f3c10b743ed5f9d492814cd67062bf93e22faae
aefayoub <aef.ayoub@gmail.com>
2025-01-01 14:41:45 +0100

    Add provider and profile route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,24 @@ import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-d
 import ProtectedRoute from './context/ProtectedRoute';
 import { AuthProvider } from './context/authContext';
 import './assets/sass/style.scss';
-import Feed from './pages/feeds';
-import LoginOrRegister from './pages/auth';
+import Footer from './containers/layout/footer';
+import Feed from './containers/feeds';
+import LoginOrRegister from './containers/auth';
+import PageNotFound from './containers/PageNotFound/PageNotFound';
+import Profile from './containers/profile';
 
 const App = () => {
   return (
     <AuthProvider>
       <Router>
         <Switch>
-          <ProtectedRoute path="/feeds" component={Feed} />
+          <ProtectedRoute path="/feeds" exact component={Feed} />
+          <ProtectedRoute path="/profile" component={Profile} />
           <Route path="/auth" component={LoginOrRegister} />
           <Redirect path="/" to="/feeds" />
+          <Route component={PageNotFound} />
         </Switch>
+        <Footer />
       </Router>
     </AuthProvider>
   );
